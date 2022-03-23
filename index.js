f7631bcd55f3ae20ee1f014b841ff85e2cedac45
WillianAbrego <willianantonioabrego@gmail.com>
2022-03-23 17:15:04 -0600

    funciones anonimas

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ function apply(num, f) {
 function double(num) {
   return 2 * num;
 }
-
+//llamando a la funcion por medio del nombre o identificador
 console.log(apply(5, double));
+
+//ejecutando un funcion anonima
+console.log(
+  apply(5, function (num) {
+    return 2 * num;
+  })
+);
+// ejecutando arrow funtion
+console.log(
+  apply(5, (num) => {
+    return 2 * num;
+  })
+);
